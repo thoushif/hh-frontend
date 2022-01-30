3bfd511a2ed59075ba379caeca7acd04d1e39df3
Thoushif Aazam Shaik <aazam555@gmail.com>
2022-01-29 22:39:24 -0600

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to get helps!</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ pages/events/add.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddPage() {
+  return (
+    <div>
+      <h1>Events - Add</h1>
+    </div>
+  );
+}
+
+export default AddPage;
